eddc278c6e84dc099487f39fa9e61b58e59cdf78
qianhao <ianloe666@gmail.com>
2020-08-11 11:57:08 -0500

    all

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import reduxThunk from 'redux-thunk'
+import axios from 'axios'
 
 import reducer from './reducers'
 import App from './components/index'
@@ -15,28 +16,86 @@ import 'semantic-ui-css/semantic.min.css'
 
 
 
-const initState = {
-    auth: {
-        authenticated: localStorage.getItem('token')
+const start = async () => {
+    const IP = process.env.REACT_APP_SERVER
+    let response = null
+    try {
+        response = await axios.get(`${IP}`, {
+            headers: {
+                Authorization: localStorage.getItem('token')
+            }
+        })
+    } catch(e) {
+        console.log(e)
     }
+
+
+    console.log("response", response)
+    const initState = {
+        auth: {
+            authenticated: response === null ? response : response.data
+        }
+    }
+    
+    
+    const store = createStore(reducer, initState, applyMiddleware(reduxThunk))
+    
+    ReactDOM.render(
+        <Auth0Provider
+            domain="qianhao.us.auth0.com"
+            clientId="n7SUj53FMNjQq226jttnVYSbRYl8kIVb"
+            // redirectUri={window.location.origin}
+            redirectUri={process.env.REACT_APP_AUTH0}
+        >
+            <Provider store={store}>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </Provider>
+        </Auth0Provider>
+        , document.querySelector('#root')
+    )
+    
+
+
 }
 
+start()
 
 
-const store = createStore(reducer, initState, applyMiddleware(reduxThunk))
 
-ReactDOM.render(
-    <Auth0Provider
-        domain="qianhao.us.auth0.com"
-        clientId="n7SUj53FMNjQq226jttnVYSbRYl8kIVb"
-        // redirectUri={window.location.origin}
-        redirectUri={process.env.REACT_APP_AUTH0}
-    >
-        <Provider store={store}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </Provider>
-    </Auth0Provider>
-    , document.querySelector('#root')
-)
+
+
+
+
+
+
+
+
+
+// ======
+//  const initState = {
+//         auth: {
+//             authenticated: localStorage.getItem('token')
+//         }
+//     }
+    
+    
+    
+//     const store = createStore(reducer, initState, applyMiddleware(reduxThunk))
+    
+//     ReactDOM.render(
+//         <Auth0Provider
+//             domain="qianhao.us.auth0.com"
+//             clientId="n7SUj53FMNjQq226jttnVYSbRYl8kIVb"
+//             // redirectUri={window.location.origin}
+//             redirectUri={process.env.REACT_APP_AUTH0}
+//         >
+//             <Provider store={store}>
+//                 <BrowserRouter>
+//                     <App/>
+//                 </BrowserRouter>
+//             </Provider>
+//         </Auth0Provider>
+//         , document.querySelector('#root')
+//     )
